6ba3615bef2631079726393d2b4aaad82e6a6a98
Eric Chabaneix <eric.chabaneix@voxpass.fr>
2019-07-10 09:52:21 +0200

    first commit staging

--- src/dialogs/livraison-dialog.js
+++ src/dialogs/livraison-dialog.js
@@ -1,27 +1,13 @@
 const { PromptDialog } = require('botfuel-dialog');
 
 class Livraison extends PromptDialog {
-  async dialogWillComplete(userMessage, data) {
-    if (data.missingEntities.size === 0) {
-      return this.triggerNext('thanks');
+    async dialogWillComplete(userMessage, data) {
+        if (data.missingEntities.size === 0) {
+            return this.triggerNext('thanks');
+        }
+        return this.wait();
     }
-    return this.wait();
-  }
 }
 
-Livraison.params = {
-  namespace: 'livraison',
-  entities: {
-    savoir: {
-      dim: 'savoir',
-    },
-    destination: {
-      dim: 'lieu',
-    },
-    temps: {
-      dim: 'quand',
-    },
-  },
-};
 
 module.exports = Livraison;
