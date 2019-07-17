520b257ae1ed87daadd6384d64deeeef784b89cd
Eric Chabaneix <eric.chabaneix@voxpass.fr>
2019-07-17 11:28:23 +0200

    error savoir-extractor modified

--- src/extractors/savoir-extractor.js
+++ src/extractors/savoir-extractor.js
@@ -3,15 +3,16 @@ const { CorpusExtractor, FileCorpus } = require('botfuel-dialog');
 class SavoirExtractor extends CorpusExtractor {
     constructor() {
         super({
-                dimension: 'savoir',
-                corpus: [
-                    new FileCorpus(`${__dirname}/../corpora/savoir.txt`),
-                    new FileCorpus(`${__dirname}/../corpora/lieu.txt`),
-                    new FileCorpus(`${__dirname}/../corpora/quand.txt`),
+            dimension: 'savoir',
+            corpus: [
+                new FileCorpus(`${__dirname}/../corpora/savoir.txt`),
+                new FileCorpus(`${__dirname}/../corpora/lieu.txt`),
+                new FileCorpus(`${__dirname}/../corpora/quand.txt`)
+            ],
 
-                    options: {},
-                });
-        }
+            options: {},
+        });
     }
+}
 
-    module.exports = SavoirExtractor;
+module.exports = SavoirExtractor;
